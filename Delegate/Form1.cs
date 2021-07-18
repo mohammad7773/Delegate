5fff493e53d571127fef332fd6a7c77a1d71a09e
mohammad7773 <mohammad.rj2707@gmail.com>
2021-07-18 11:32:54 +0430

    Add project files.

---
+++ Delegate/Form1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Delegate
+{
+    public partial class Form1 : Form
+    {
+       StringInput oneInput=new StringInput();
+        StringTwoInpute twoInpute=new StringTwoInpute();
+        Inpute inpt =new Inpute();
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+           
+        }
+    }
+}
---
+++ Delegate/StringInput.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Delegate
+{
+    public delegate void Inpute(string name);
+    public class StringInput
+    {
+        
+        public Inpute GetInpute;
+
+        public void InputeString(string word)
+        {
+            var wordReplace = Regex.Replace(word, ".{1}", "$0.");
+            var wordsplit = wordReplace.Split(Convert.ToChar(".")).ToArray();
+            string[] wordarray = new string[] { };
+            for (int i = 0; i < wordsplit.Length - 1; i++)
+            {
+                wordarray[i] = wordsplit[i];
+            }
+            GetInpute(wordarray.ToString());
+        }
+    }
+
+    public class StringTwoInpute
+    {
+        public void InputeStringTwo(string word)
+        {
+
+        }
+
+
+    }
+}
